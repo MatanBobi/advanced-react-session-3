0e0a094ebb40ff7f11a3350522435455940b9422
Matan Borenkraout <matanbobi@gmail.com>
2023-01-02 12:38:25 +0200

    remove a redundant library

--- src/PokemonModal.tsx
+++ src/PokemonModal.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 
 export function PokemonModal({ pokemonId }: { pokemonId: string }) {
   const [pokemonDetails, setPokemonDetails] = useState<any>();
