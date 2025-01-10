10d41aa8b7ead7869ce0061a6b5be331cace74b0
Matan Borenkraout <matanbobi@gmail.com>
2025-01-10 13:55:43 +0200

    modal fixes

--- src/Modal/ModalContent.tsx
+++ src/Modal/ModalContent.tsx
@@ -11,20 +11,12 @@ export function ModalContent({
     setIsOpen(false);
   }
 
+  if (!isOpen) {
+    return null;
+  }
+
   return (
-    <div
-      style={{
-        padding: "15px",
-        boxShadow: "3px 3px 5px 6px #ccc",
-        borderRadius: "10px",
-        position: "fixed",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        display: `${isOpen ? "block" : "none"}`,
-        backgroundColor: "black",
-      }}
-    >
+    <div className="shadow-xl p-4 rounded-lg fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black block">
       <button onClick={closeModal}>X</button>
       <div className="modal-content">{children}</div>
     </div>
--- src/PokemonModal.tsx
+++ src/PokemonModal.tsx
@@ -1,31 +1,36 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { Loader } from "./Loader";
+
+const getPokemonDetails = async (pokemonId: string) => {
+  const result = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
+  ).then((res) => res.json());
+  return result;
+};
 
 export function PokemonModal({ pokemonId }: { pokemonId: string }) {
-  const [pokemonDetails, setPokemonDetails] = useState<any>();
-
-  useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setPokemonDetails(data);
-      });
-  }, [pokemonId]);
+  const { data: pokemonDetails = {}, isLoading } = useQuery({
+    queryKey: ["selectedPokemon", pokemonId],
+    queryFn: () => getPokemonDetails(pokemonId),
+  });
 
   return (
-    <div className="pokemon-modal">
-      <div className="pokemon-modal-image">
-        {pokemonDetails && pokemonDetails.sprites && (
-          <img
-            src={pokemonDetails.sprites.front_default}
-            alt={pokemonDetails.name}
-          />
-        )}
+    <Loader isLoading={isLoading}>
+      <div className="pokemon-modal">
+        <div className="pokemon-modal-image">
+          {pokemonDetails && pokemonDetails.sprites && (
+            <img
+              src={pokemonDetails.sprites.front_default}
+              alt={pokemonDetails.name}
+            />
+          )}
+        </div>
+        <div>
+          <h1>{pokemonDetails?.name}</h1>
+          <p>Height: {pokemonDetails?.height}</p>
+          <p>Weight: {pokemonDetails?.weight}</p>
+        </div>
       </div>
-      <div>
-        <h1>{pokemonDetails?.name}</h1>
-        <p>Height: {pokemonDetails?.height}</p>
-        <p>Weight: {pokemonDetails?.weight}</p>
-      </div>
-    </div>
+    </Loader>
   );
 }
