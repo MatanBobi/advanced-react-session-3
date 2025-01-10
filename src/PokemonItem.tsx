f1543f4d156899af5ddf7e477adf8a278c73bd02
Matan Borenkraout <matanbobi@gmail.com>
2025-01-10 13:59:25 +0200

    working solution

--- src/PokemonItem.tsx
+++ src/PokemonItem.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Pokemon } from "./types";
 import { getMainImageUrl } from "./utils";
 import { Modal } from "./Modal/Modal";
@@ -6,43 +7,45 @@ import { ModalContent } from "./Modal/ModalContent";
 import { PokemonModal } from "./PokemonModal";
 import { InfoIcon } from "./assets/InfoIcon";
 
-export function PokemonItem({
-  pokemon,
-  onChange,
-  isCaught,
-}: {
-  pokemon: Pokemon;
-  onChange: (pokemon: Pokemon, caught: boolean) => void;
-  isCaught: boolean;
-}) {
-  return (
-    <div className="px-4 py-5 border-t border-t-slate-300 hover:bg-slate-200 font-medium dark:border-t-gray-600 dark:text-white dark:hover:bg-gray-700 dark:bg-gray-800 flex items-center justify-between">
-      <div className="flex gap-2 items-center">
-        <img
-          className="w-10"
-          src={getMainImageUrl(pokemon.name)}
-          alt={pokemon.name}
-        />
-        <span className="capitalize">{pokemon.name}</span>
+export const PokemonItem = React.memo(
+  ({
+    pokemon,
+    onChange,
+    isCaught,
+  }: {
+    pokemon: Pokemon;
+    onChange: (pokemon: Pokemon, caught: boolean) => void;
+    isCaught: boolean;
+  }) => {
+    return (
+      <div className="px-4 py-5 border-t border-t-slate-300 hover:bg-slate-200 font-medium dark:border-t-gray-600 dark:text-white dark:hover:bg-gray-700 dark:bg-gray-800 flex items-center justify-between">
+        <div className="flex gap-2 items-center">
+          <img
+            className="w-10"
+            src={getMainImageUrl(pokemon.name)}
+            alt={pokemon.name}
+          />
+          <span className="capitalize">{pokemon.name}</span>
+        </div>
+        <div className="flex gap-2">
+          <input
+            type="checkbox"
+            className="cursor-pointer"
+            checked={isCaught}
+            onChange={() => {
+              onChange(pokemon, !isCaught);
+            }}
+          />
+          <Modal>
+            <ModalButton>
+              <InfoIcon style={{ height: 24, width: 24 }} alt="info" />
+            </ModalButton>
+            <ModalContent>
+              <PokemonModal pokemonId={pokemon.name} />
+            </ModalContent>
+          </Modal>
+        </div>
       </div>
-      <div className="flex gap-2">
-        <input
-          type="checkbox"
-          className="cursor-pointer"
-          checked={isCaught}
-          onChange={() => {
-            onChange(pokemon, !isCaught);
-          }}
-        />
-        <Modal>
-          <ModalButton>
-            <InfoIcon style={{ height: 24, width: 24 }} alt="info" />
-          </ModalButton>
-          <ModalContent>
-            <PokemonModal pokemonId={pokemon.name} />
-          </ModalContent>
-        </Modal>
-      </div>
-    </div>
-  );
-}
+    );
+  }
+);
--- src/Pokemons.tsx
+++ src/Pokemons.tsx
@@ -1,12 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { matchSorter } from "match-sorter";
 import { Header } from "./Header";
 import { PokemonItem } from "./PokemonItem";
 import { Pokemon } from "./types";
-import { withLoader } from "./withLoader";
 import { useForceRerender } from "./useForceRerender";
 import { Loader } from "./Loader";
-import { useQuery } from "@tanstack/react-query";
+import { create } from "zustand";
 
 const fetchPokemons = async (): Promise<Pokemon[]> => {
   const result = await fetch(
@@ -15,36 +14,55 @@ const fetchPokemons = async (): Promise<Pokemon[]> => {
   return result.results;
 };
 
+interface PokemonState {
+  pokemons: Pokemon[];
+  isLoading: boolean;
+  fetchPokemons: () => Promise<void>;
+}
+const usePokemonsStore = create<PokemonState>((set) => ({
+  pokemons: [],
+  isLoading: false,
+  fetchPokemons: async () => {
+    set({ isLoading: true });
+    const results = await fetchPokemons();
+    set({ pokemons: results, isLoading: false });
+  },
+}));
+
 export function Pokemons() {
-  const {
-    data: pokemons = [],
-    isLoading,
-    isError,
-  } = useQuery({
-    queryKey: ["pokemons"],
-    queryFn: fetchPokemons,
-  });
+  const isLoading = usePokemonsStore((state) => state.isLoading);
+  const pokemons = usePokemonsStore((state) => state.pokemons);
+  const fetchPokemons = usePokemonsStore((state) => state.fetchPokemons);
   const [caughtPokemons, setCaughtPokemons] = useState<Pokemon[]>([]);
   const [searchTerm, setSearchTerm] = useState("");
   const forceRerender = useForceRerender();
 
-  let visiblePokemons = pokemons.length
-    ? matchSorter(pokemons, searchTerm, { keys: ["name"] })
-    : [];
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
 
-  const handlePokemonCaught = (pokemon: Pokemon, caught: boolean) => {
-    setCaughtPokemons((prev) => {
-      if (caught) {
-        if (prev.includes(pokemon)) {
-          return prev;
+  const visiblePokemons = React.useMemo(() => {
+    return pokemons.length
+      ? matchSorter(pokemons, searchTerm, { keys: ["name"] })
+      : [];
+  }, [pokemons, searchTerm]);
+
+  const handlePokemonCaught = React.useCallback(
+    (pokemon: Pokemon, caught: boolean) => {
+      setCaughtPokemons((prev) => {
+        if (caught) {
+          if (prev.includes(pokemon)) {
+            return prev;
+          }
+
+          return [...prev, pokemon];
+        } else {
+          return prev.filter((item) => item !== pokemon);
         }
-
-        return [...prev, pokemon];
-      } else {
-        return prev.filter((item) => item !== pokemon);
-      }
-    });
-  };
+      });
+    },
+    []
+  );
 
   return (
     <div className="max-h-screen flex flex-col border-r border-r-slate-300 dark:border-r-slate-600 dark:bg-gray-800 overflow-auto">
@@ -55,7 +73,7 @@ export function Pokemons() {
         onChangeSearch={setSearchTerm}
         forceRerender={forceRerender}
       />
-      <Loader isLoading={isLoading} isError={isError}>
+      <Loader isLoading={isLoading}>
         {visiblePokemons.map((pokemon) => (
           <PokemonItem
             key={pokemon.name}
